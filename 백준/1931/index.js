c4f19b0ac5e064bcd04901a6d3e15c687c43c042
JBS <jungbeomsu@naver.com>
2023-04-09 22:33:38 +0900

    add: 1931번 회의실 배정 해결

--- 백준/1931/index.js
+++ 백준/1931/index.js
@@ -1,10 +1,27 @@
 const fs = require('fs');
 
-const [cnt, ...meetings] = fs
+const [total, ...meetings] = fs
   .readFileSync('백준/1931/input.txt')
   .toString()
   .split('\n');
 
-const as = meetings.map(el => el.split(' '));
+let rooms = meetings
+  .map(el => el.split(' ').map(Number))
+  .sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    }
+    return a[1] - b[1];
+  });
 
-console.log(as);
+let cnt = 1;
+let end = rooms[0][1];
+
+for (let i = 1; i < total; i += 1) {
+  if (rooms[i][0] >= end) {
+    cnt += 1;
+    end = rooms[i][1];
+  }
+}
+
+console.log(cnt);
