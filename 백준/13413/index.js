b252f57a83cbe9efe5200683468320c553f64465
JBS <jungbeomsu@naver.com>
2023-04-30 03:53:49 +0900

    [Silver 4] 오셀로 재배치 수정

--- 백준/13413/index.js
+++ 백준/13413/index.js
@@ -1,33 +1,54 @@
 const fs = require('fs');
 const PATH =
   process.platform === 'linux' ? '/dev/stdin' : '백준/13413/input.txt';
-const [totalCount, ...input] = fs.readFileSync(PATH).toString().split('\n');
+const [totalCount, ...input] = fs
+  .readFileSync(PATH)
+  .toString()
+  .trim()
+  .split('\n');
 
 for (let i = 0; i < input.length; i += 3) {
-  const start = input[i + 1];
-  const end = input[i + 2];
+  const start = input[i + 1].split('');
+  const end = input[i + 2].split('');
+  let ans = 0;
 
-  const startWlen = start.replaceAll('B', '').length;
-  const startBlen = start.replaceAll('W', '').length;
-  const endWlen = end.replaceAll('B', '').length;
-  const endBlen = end.replaceAll('W', '').length;
+  const sw = start.filter(el => el === 'W').length;
+  const sb = start.filter(el => el === 'B').length;
+  const ew = end.filter(el => el === 'W').length;
+  const eb = end.filter(el => el === 'B').length;
 
-  let answer = 0;
+  const minW = sw > ew ? sw - ew : ew - sw;
+  const minB = sb > eb ? sb - eb : eb - sb;
 
-  console.log(startWlen, startBlen, endWlen, endBlen);
+  let min = Math.min(minW, minB);
 
-  const minW = startWlen > endWlen ? startWlen - endWlen : endWlen - startWlen;
-  const minB = startBlen > endBlen ? startBlen - endBlen : endBlen - startBlen;
+  for (let j = 0; j < start.length; j += 1) {
+    if (min === 0) break;
 
-  if (startWlen === endWlen && startBlen === endBlen) {
-    for (let j = 0; j < start.length; j++) {
-      if (start[j] === 'W' && start[j] !== end[j]) {
-        answer += 1;
+    if (start[j] !== end[j]) {
+      if (min) {
+        min -= 1;
       }
+      ans += 1;
+      start[j] = start[j] === 'W' ? 'B' : 'W';
     }
-  } else {
-    answer = minW !== minB ? minW + minB : minW;
   }
 
-  console.log(answer);
+  for (let j = 0; j < start.length; j += 1) {
+    if (start[j] !== end[j]) {
+      ans += 0.5;
+    }
+  }
+
+  if (
+    !start.includes('W') ||
+    !start.includes('B') ||
+    !end.includes('W') ||
+    !end.includes('B')
+  ) {
+    console.log(ans);
+    continue;
+  }
+
+  console.log(ans);
 }
