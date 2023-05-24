1ab51fcfaf2e55e8b08c512a8d35985ea52719ca
JBS <jungbeomsu@naver.com>
2023-05-24 19:53:10 +0900

    [Silver 4] 1748번 수 이어쓰기

---
+++ 백준/1748/index.py
@@ -0,0 +1,11 @@
+N = input()
+
+comp = len(N) - 1
+result = 0
+
+for i in range(comp):
+  result += 9 * (10 ** i) * (i + 1)
+  i += 1
+  
+result += ((int(N) - (10 ** comp)) + 1) * (comp + 1)
+print(result)
